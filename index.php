4b9dc210c984b7275b1f6b29e44ef405685b2004
mimietitou <mragot2@msn.com>
2016-11-23 12:34:35 +0100

    michelecss

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Projet commun</title>
+  </head>
+  <body>
+    <h1>https://github.com/mimietitou/projetcommun/invitations</h1>
+  </body>
+</html>
