bc07551341c2b119a6cf3511a967c8507ec17340
franckbacquetwebforce <franckbacquet.formation@gmail.com>
2016-11-23 12:27:54 +0100

    ajout lien css

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
     <title></title>
   </head>
   <body>
     <h1> ceci est un titre</h1>
+
+
   </body>
 </html>
