7b5ef61938a3b9186d7c81ca79af214901eae648
mimietitou <mragot2@msn.com>
2016-11-23 13:57:37 +0100

    modif contact

--- contact.php
+++ contact.php
@@ -1,1 +1,42 @@
 <?php echo 'salut'; ?>
+
+<!-- FORMULAIRE d'inscription avec vérification des erreurs-->
+<div class="container">
+  <div class="row">
+    <div class="modal-content">
+      <div class="modal-header">
+      <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times"></i></button>
+      <h4 class="modal-title">Creer un nouveau compte</h4>
+    </div>
+    <div id="inscriptiondone"></div>
+      <form id="inscription" action="inscription_ajax.php" method="post">
+        <div class="modal-body">
+          <div class="form-group">
+            <label class="form-control" for="pseudo">Pseudo*</label><br><br>
+            <input type="text" name="pseudo" value=""><br>
+            <span class="help-block" id="error_pseudo"></span><br>
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="email">Email*</label><br><br>
+                <input type="email" name="email" value=""><br>
+                <span class="help-block" id="error_email"></span><br>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <textarea type="text" name="message" value=""></textarea><br>
+                <span class="help-block" id="error_password"></span><br>
+              </div>
+            </div>
+          </div>
+        </div>
+      <div class="modal-footer">
+        <input type="hidden" name="isEmpty" value="">
+        <button type="submit" name="submit" value="Soumettre" class="btn btn-success btn-icon"><i class="fa fa-check"></i>Soumettre</button>
+      </div>
+    </form>
+  </div>
+</div>
+</div>
