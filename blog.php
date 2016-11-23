0e119e0de8b6b5302fd3095e4250fcbef33bbaa8
laurent76 <lau76120@gmail.com>
2016-11-23 16:51:51 +0100

    passe de lolo a master

--- blog.php
+++ blog.php
@@ -30,7 +30,7 @@ include('include/header.php');
                          </p>
                              <p></p>
                          </article>
-                     <a class="btn btn-blog pull-right marginBottom10" href="http://bootsnipp.com/user/snippets/2RoQ">More Information</a>
+                     <a class="btn btn-blog pull-right marginBottom10" href="http://bootsnipp.com/user/snippets/2RoQ">Ne click pas</a>
                  </div>
                   <div class="col-md-9 blogShort">
                      <h1>Title 2</h1>
@@ -42,7 +42,7 @@ include('include/header.php');
                          of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of
                          Lorem Ipsum.
                          </p></article>
-                     <a class="btn btn-blog pull-right marginBottom10" href="http://bootsnipp.com/user/snippets/2RoQ">READ MORE</a>
+                     <a class="btn btn-blog pull-right marginBottom10" href="http://bootsnipp.com/user/snippets/2RoQ">Ne click pas</a>
                  </div>
 
                   <div class="col-md-9 blogShort">
@@ -55,7 +55,7 @@ include('include/header.php');
                          of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of
                          Lorem Ipsum.
                          </p></article>
-                     <a class="btn btn-blog pull-right marginBottom10" href="http://bootsnipp.com/user/snippets/2RoQ">READ MORE</a>
+                     <a class="btn btn-blog pull-right marginBottom10" href="http://bootsnipp.com/user/snippets/2RoQ">Ne click pas</a>
                  </div>
 
                <div class="col-md-12 gap10"></div>
