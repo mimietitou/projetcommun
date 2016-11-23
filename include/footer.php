fe4f3236141347fc55bb4d2b0529fb994222ed6f
Franck Bacquet <franckbacquet.pro@gmail.com>
2016-11-23 19:40:41 +0100

    Modification des scripts du footer et ajout des dossiers js et font

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,6 @@
 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="js/jquery-3.1.1.js"></script>
+<script src="js/bootstrap.js"></script>
 <script>$('.carousel').carousel()</script>
   </body>
 </html>
